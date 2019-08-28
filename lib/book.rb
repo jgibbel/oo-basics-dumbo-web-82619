7d8b28b6a4737b10a96e1b48f5a5686e8d8deeee
James Gibbel <jamesgibbel@gmail.com>
2019-08-28 14:41:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Book 
+  def initialize(title)
+    @title = title 
+  end 
+  
+  attr_accessor :title 
+  attr_accessor :author 
+  attr_accessor :page_count
+  attr_accessor :genre 
+  attr_accessor :turn_page
+end 
 
