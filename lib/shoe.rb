e38208c7422bf76b3a730ad7687bc81cb3579422
James Gibbel <jamesgibbel@gmail.com>
2019-08-28 14:46:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 # Make your shoe class here!
-class Book 
-  def initialize(title)
-    @title = title 
+class Shoe
+  def initialize(brand)
+    @brand = brand
   end 
   
-  attr_reader :title 
-  attr_accessor :author, :page_count, :genre 
-  
-  def turn_page 
-    puts "Flipping the page...wow, you read fast!"
-  end 
+  attr_reader :brand 
+  attr_accessor :color, :size, :material, :condition 
+
   
 end 
